f8c592ef582464383e7ea6e1eec2779793341c79
Malika <malika237581@gmail.com>
2026-06-14 18:53:51 +0530

    Update

--- src/hooks/useSocket.jsx
+++ src/hooks/useSocket.jsx
@@ -1,10 +1,10 @@
 import { io } from "socket.io-client";
 
 // Development
-const SERVER_URL = "http://localhost:3000";
+// const SERVER_URL = "http://localhost:3000";
 
 // Production example:
-// const SERVER_URL = "https://snake-game-1pvr.onrender.com";
+const SERVER_URL = "https://snake-game-1-99x5.onrender.com/";
 
 const socket = io(SERVER_URL, {
     autoConnect: false,
