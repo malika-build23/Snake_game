909baac9a43279f17b3b81305dfd3ddbd245094f
Malika <malika237581@gmail.com>
2026-06-14 17:49:50 +0530

    Use Render backend

--- src/hooks/useSocket.jsx
+++ src/hooks/useSocket.jsx
@@ -1,10 +1,10 @@
 import { io } from "socket.io-client";
 
 // Development
-const SERVER_URL = "http://localhost:3000";
+
 
 // Production example:
-// const SERVER_URL = "https://your-render-server.onrender.com";
+const SERVER_URL = "https://snake-game-1pvr.onrender.com";
 
 const socket = io(SERVER_URL, {
     autoConnect: false,
